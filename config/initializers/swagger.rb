c1bb9cea9fb909bc7edce5eca68d35158062d820
suixuexiang <suixuexiang@huobi.com>
2020-07-05 13:54:32 +0800

    [hotfix]

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,2 +1,2 @@
 GrapeSwaggerRails.options.url      = '/api/v1/doc/swagger_doc.json'
-GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
+GrapeSwaggerRails.options.app_url  = "#{ENV['RENT_SERVER_PROTOCOL']}://#{ENV['RENT_SERVER_HOST']}:#{ENV['RENT_SERVER_PORT']}"

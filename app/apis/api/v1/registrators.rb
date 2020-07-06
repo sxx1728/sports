3dd999b7186ec4116af6fee70d6c1f7a79ecfb9e
suixuexiang <suixuexiang@huobi.com>
2020-07-06 22:11:05 +0800

    [hotfix]

--- app/apis/api/v1/registrators.rb
+++ app/apis/api/v1/registrators.rb
@@ -14,17 +14,18 @@ module API
             app_error('无效类型', 'invalid type') unless User.valid_type? params[:type]
             app_error('无效电话', 'invalid phone') unless Captcha.valid_phone? params[:phone]
             app_error('无效验证码', 'invalid captcha') unless Captcha.valid_captcha? params[:captcha]
-            app_error('验证码错误', 'captcha incorrect') unless Captcha.where(phone: params[:phone], captcha: params[:captcha])
-              .where("expire_at > ?", DateTime.current).exists?
+            app_error('验证码错误', 'captcha incorrect') unless Captcha.where(phone: params[:phone], captcha: params[:captcha]).exists?
             app_error('无效密码', 'password_md5 incorrect') unless params[:password_md5].present?
-            Captcha.where(phone: params[:phone], captcha: params[:captcha]).where("expire_at > ?", DateTime.current)
-              .update_all(expire_at: DateTime.current)
+
+            captcha = Captcha.where(phone: params[:phone], captcha: params[:captcha]).last;
+            app_error('验证码已过期', 'captcha expired') unless captcha.expire_at >  DateTime.current
 
             app_error('手机号已占用', 'phone ocupied') if User.exists?(phone: params[:phone])
             "#{params[:type].capitalize}::User".constantize.create(phone: params[:phone],
                         password_md5: params[:password_md5], 
                         nick_name: User.generate_nick_name)
 
+            captcha.update(expire_at: DateTime.current)
             present 'succeed'
           end
 
